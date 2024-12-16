ca898db543481f6573977dee0522fb6f45181e9c
Chikhaliya Vaibhav <chikhaliyarekha20@gmail.com>
2024-12-16 11:49:35 +0530

    Add files via upload

---
+++ Lec 1/index.js
@@ -0,0 +1,32 @@
+console.log('he is a good boy'); 
+
+const myObject = {  
+    name: 'John',
+    age: 30,
+    occupation: 'Developer',
+    hobbies: ['reading', 'swimming', 'coding'],
+    address: {
+        street: '123 Main St',
+        city: 'Anytown',
+        state: 'CA',
+        zip: '12345'
+        }
+        };
+        console.log(myObject);
+
+let myArray = [1, 2, 3, 4, 5];
+console.log(myArray);
+
+const mySet = new Set([1, 2, 3, 4, 5]);
+console.log(mySet);
+
+const sum = () => {
+    const a=10;
+    const b=20;
+    const c=a+b;
+    const d=c*2;
+    return d;
+}; 
+console.log(sum());
+
+ let arr = [1, 2, 3, 4, 5];
